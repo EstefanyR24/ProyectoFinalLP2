6ef0dc0f74fa993c68c001f09215dce54e97e9ef
Brenda Estefany Reyes Jeri <I202114794@cibertec.edu.pe>
2025-10-26 00:44:09 -0500

    subir cambios

---
+++ EcommerceSCE/EcommerceSCE/src/main/java/com/ecommerce/sce/EcommerceSceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.sce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSceApplication.class, args);
+	}
+
+}
