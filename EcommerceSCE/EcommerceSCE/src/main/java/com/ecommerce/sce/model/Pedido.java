62e4dab5ffc673a7f41f061d622413beee07b04f
Luis Alfredo Palomino Medina <pmpalominomedina@gmail.com>
2025-10-27 21:40:48 -0500

    subida

--- EcommerceSCE/EcommerceSCE/src/main/java/com/ecommerce/sce/model/Pedido.java
+++ EcommerceSCE/EcommerceSCE/src/main/java/com/ecommerce/sce/model/Pedido.java
@@ -1,5 +1,15 @@
 package com.ecommerce.sce.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "pedido")
 public class Pedido {
 
+	
+	
+	
 }
