54ba27f55b13d3f1650bbf60c6b986afc69dace0
Luis Alfredo Palomino Medina <pmpalominomedina@gmail.com>
2025-10-29 19:03:21 -0500

    prueba de pulll

--- EcommerceSCE/EcommerceSCE/src/main/java/com/ecommerce/sce/controller/PedidoController.java
+++ EcommerceSCE/EcommerceSCE/src/main/java/com/ecommerce/sce/controller/PedidoController.java
@@ -1,5 +1,10 @@
 package com.ecommerce.sce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class PedidoController {
+
+	private final Logger LOGGER = LoggerFactory.getLogger(CotizacionController.class);
 
 }
