07b16858adcb92cfb7dbb4c6f7acb9f538aacc70
Brenda Estefany Reyes Jeri <I202114794@cibertec.edu.pe>
2025-10-29 20:22:59 -0500

    cambios de controller proveedor y producto

--- EcommerceSCE/EcommerceSCE/src/main/java/com/ecommerce/sce/controller/ProductoController.java
+++ EcommerceSCE/EcommerceSCE/src/main/java/com/ecommerce/sce/controller/ProductoController.java
@@ -1,5 +1,91 @@
 package com.ecommerce.sce.controller;
 
-public class ProductoController {
+import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ecommerce.sce.service.ProductoService;
+import com.ecommerce.sce.model.Proveedor;
+import com.ecommerce.sce.model.Productos;
+import com.ecommerce.sce.controller.ProductoController;
+
+@Controller
+@RequestMapping("/productos")
+public class ProductoController {
+	
+	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+	
+	@Autowired
+	private ProductoService productoService;
+	
+	@GetMapping("")
+	public String show(Model model) {
+		model.addAttribute("producto",productoService.findAll());
+		return "producto/show";
+	}
+	
+	
+	@GetMapping("/create")
+	public String create() {
+		return "producto/create";
+	}
+	
+	
+	@PostMapping("/save")
+	public String save(Productos productos) {
+		LOGGER.info("Este es el objeto producto {}",productos);
+		Proveedor p = new Proveedor(1,"", "");
+		productos.setProveedor(p);
+		productoService.save(productos);
+		return "redirect:/producto";
+	}
+	
+	
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable Integer id, Model model) {
+		Productos producto= new Productos();
+		Optional<Productos> optionalProducto=productoService.get(id);
+		producto= optionalProducto.get();
+		LOGGER.info("Producto buscado: {}",producto);
+		model.addAttribute("producto", producto);
+		
+		return "producto/edit";
+	}
+	
+	
+	
+	@PostMapping("/update")
+	public String update(Productos producto) {
+		productoService.update(producto);		
+		return "redirect:/producto";
+	}
+	
+	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable Integer id) {
+		productoService.delete(id);
+		return "redirect:/producto";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- EcommerceSCE/EcommerceSCE/src/main/java/com/ecommerce/sce/controller/ProveedorController.java
+++ EcommerceSCE/EcommerceSCE/src/main/java/com/ecommerce/sce/controller/ProveedorController.java
@@ -1,5 +1,70 @@
 package com.ecommerce.sce.controller;
 
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.ecommerce.sce.service.ProveedorService;
+import com.ecommerce.sce.model.Proveedor;
+import com.ecommerce.sce.controller.ProveedorController;
+
+
+@Controller
+@RequestMapping("/proveedor")
 public class ProveedorController {
 
+	private final Logger LOGGER = LoggerFactory.getLogger(ProveedorController.class);
+	
+	@Autowired
+	private ProveedorService proveedorService;
+	
+	@GetMapping("")
+	public String show(Model model) {
+		model.addAttribute("proveedor" , proveedorService.findAll());
+		return "proveedor/show";
+		
+		
+	}
+		@GetMapping("/create")
+		public String create() {
+			return "proveedor/create";
+		}
+		
+		
+		@PostMapping("/save")
+		public String save(Proveedor proveedor) {
+			LOGGER.info("Este es el objeto:", proveedor);
+			proveedorService.save(proveedor);
+			return "redirect:/proveedor";
+		}
+		
+		
+		@GetMapping("/edit/{id}")
+		public String edit(@PathVariable Integer id,Model model){
+			Proveedor proveedor = new Proveedor();
+			Optional<Proveedor> optionalProveedor= proveedorService.get(id);
+			proveedor= optionalProveedor.get();
+			model.addAttribute("Proveedor buscado:{}", proveedor);
+			return "proveedor/edit";
+	
+	}
+		
+		
+		@PostMapping("/update")
+		public String update(Proveedor proveedor) {
+			proveedorService.update(proveedor);
+			return "redirect:/proveedor";
+			
+		
+		}
+		
+	
+	
 }
